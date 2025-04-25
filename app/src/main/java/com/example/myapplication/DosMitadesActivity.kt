30d57b16d5c7c5ab5811959797bb2b8eab05f4c6
Carlos Sutachan Parra <carlos.sutachanp@utadeo.edu.co>
2025-04-24 21:00:24 -0500

    pantalla de inicio y de login con intents y campos mandatorios2

--- app/src/main/java/com/example/myapplication/DosMitadesActivity.kt
+++ app/src/main/java/com/example/myapplication/DosMitadesActivity.kt
@@ -7,7 +7,7 @@ import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
-class dosmitadesActivity : AppCompatActivity() {
+class DosMitadesActivity : AppCompatActivity() {
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
